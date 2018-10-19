8161225f712e76f26cb2b3b9e438142f5a8c245f
tachometer74 <dneil.crawford@gmail.com>
2018-10-18 21:26:37 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/home/amp/ampinstmgr CreateInstance ADS ADS-Instance 0.0.0.0 8080 ${license} ${password} +Core.Login.Username ${username}
-/home/AMP/ampinstmgr StartInstance ADS-Instance
-/home/AMP/ampinstmgr View ADS-Instance
+/home/amp/ampinstmgr CreateInstance ${MODULE} ${INSTANCE_NAME} 0.0.0.0 8080 ${LICENCE} ${PASSWORD} +Core.Login.Username ${PASSWORD}
+/home/AMP/ampinstmgr StartInstance ${INSTANCE_NAME}
+/home/AMP/ampinstmgr View ${INSTANCE_NAME}
